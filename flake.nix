bc698a50a6034202a979f2047b1faec74c8d81b8
Xida Ren <cedar.ren@gmail.com>
2024-09-10 00:47:19 -0700

    new flake

--- flake.nix
+++ flake.nix
@@ -1,50 +1,80 @@
 {
-  description = "NixOS configuration";
+  description = "Your new nix config";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    home-manager = {
-      url = "github:nix-community/home-manager";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    # Nixpkgs
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
+    # You can access packages and modules from different nixpkgs revs
+    # at the same time. Here's an working example:
+    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
+    # Also see the 'unstable-packages' overlay at 'overlays/default.nix'.
+
+    # Home manager
+    home-manager.url = "github:nix-community/home-manager/release-24.05";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { self, nixpkgs, home-manager, ... }@inputs:
-    let
-      system = "x86_64-linux";
-      pkgs = nixpkgs.legacyPackages.${system};
-
+  outputs = {
+    self,
+    nixpkgs,
+    home-manager,
+    ...
+  } @ inputs: let
+      inherit (self) outputs;
       lib = nixpkgs.lib;
+      systems = [
+      "aarch64-linux"
+      "i686-linux"
+      "x86_64-linux"
+      "aarch64-darwin"
+      "x86_64-darwin"
+    ];
+    # This is a function that generates an attribute by calling a function you
+    # pass to it, with each system as an argument
+    forAllSystems = nixpkgs.lib.genAttrs systems;
+  in {
+    overlays = import ./overlays { inherit inputs; };
+    packages = forAllSystems (system: import ./pkgs nixpkgs.legacyPackages.${system});
 
-      # Define the list of hosts
-      hosts = [
-        { name = "solderpad"; username = "cedar"; }
-        # Add more hosts here as needed
+    formatter = forAllSystems (system: nixpkgs.legacyPackages.${system}.alejandra);
+      
+    nixosConfigurations = {
+      commonModules = [
+        ./nixos/configuration.nix
+        home-manager.nixosModules.home-manager
+        {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+        }
+        {
+          nixpkgs = {
+            overlays = [
+              outputs.overlays.additions
+              outputs.overlays.modifications
+              outputs.overlays.unstable-packages
+            ];
+            config = {
+              allowUnfree = true;
+            };
+          };
+        }
       ];
 
-      # Function to generate host configurations
-      mkHost = { name, username }: lib.nixosSystem {
-        inherit system;
-        modules = [
-          # Common NixOS configuration
-          ./nixos/configuration.nix
-
-          # Host-specific configuration
-          (import ./hosts/${name}/configuration.nix { inherit name username; })
-
-          # Home-manager configuration
-          home-manager.nixosModules.home-manager
+      # Define hosts
+      solderpad = lib.nixosSystem {
+        system = "x86_64-linux";
+        specialArgs = { inherit inputs outputs; };
+        modules = commonModules ++ [
+          ./hosts/solderpad/configuration.nix
           {
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-            home-manager.users.${username} = import ./home-manager/home.nix;
+            networking.hostName = "solderpad";
+            home-manager.users.cedar = import ./home-manager/home.nix;
           }
         ];
       };
-    in
-    {
-      nixosConfigurations = builtins.listToAttrs (map
-        (host: lib.nameValuePair host.name (mkHost host))
-        hosts);
+
+      # You can add more hosts here following the same pattern
+      # examplehost = lib.nixosSystem { ... };
     };
+  };
 }
