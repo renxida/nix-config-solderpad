3262cc050f1c58013a9fb22661e2b9a322591c21
Cedar <cedar.ren@gmail.com>
2024-09-10 23:21:40 +0000

    temporarily commenting out failed-to-build remote extension

--- home-manager/vscode.nix
+++ home-manager/vscode.nix
@@ -7,11 +7,11 @@ pkgs.vscode-with-extensions.override {
     rust-lang.rust-analyzer
     github.copilot
   ] ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
-    {
-      name = "remote-server";
-      publisher = "ms-vscode";
-      version = "1.5.6";
-      sha256 = "0llrl0s9fll17sc1fxx5ndk5l9x25k4ln1gahpc7ljwhykyzg9hs"; # Replace with actual sha256
-    }
+    #{
+    #  name = "remote-server";
+    #  publisher = "ms-vscode";
+    #  version = "1.5.6";
+    #  sha256 = "0llrl0s9fll17sc1fxx5ndk5l9x25k4ln1gahpc7ljwhykyzg9hs"; # Replace with actual sha256
+    #}
   ];
 }
